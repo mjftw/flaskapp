dd3218ca14238ccfacfac86b6663aa95d629fc59
Merlin Webster <mwebster@witekio.com>
2018-11-03 10:43:05 +0000

    Added basic password hashing

--- webapp/models.py
+++ webapp/models.py
@@ -1,6 +1,9 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from webapp import db
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
@@ -8,3 +11,9 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def password_set(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def password_check(self, password):
+        return check_password_hash(self.password_hash, password)
