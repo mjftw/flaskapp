23468235c2e29659ecd42acad0689860a0f9d549
Merlin Webster <mwebster@witekio.com>
2018-11-04 23:06:05 +0000

    Revamped look of UI with bootstrap

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_bootstrap import Bootstrap
 
 from .config import Config
 
@@ -11,6 +12,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 
 from webapp import routes, errors, models
 from webapp.admin_views import UserIsAdminIndexView, UserIsAdminModelView
