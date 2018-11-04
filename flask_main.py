de3f5931535dee7058bc7736f79d470388f7bc91
Merlin Webster <mwebster@witekio.com>
2018-11-04 11:24:05 +0000

    Added user admin checking and access table

--- flask_main.py
+++ flask_main.py
@@ -1,6 +1,6 @@
 from webapp import app, db
-from webapp.models import User
+from webapp.models import User, UserAccess
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'UserAccess': UserAccess}
