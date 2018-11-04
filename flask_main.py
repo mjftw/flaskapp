3df2284c1a1bb7f72fa0161f6012304668cb83bd
Merlin Webster <mwebster@witekio.com>
2018-11-04 15:16:07 +0000

    Removed UserAccess import from flask_main

--- flask_main.py
+++ flask_main.py
@@ -1,6 +1,6 @@
 from webapp import app, db
-from webapp.models import User, UserAccess
+from webapp.models import User
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'UserAccess': UserAccess}
+    return {'db': db, 'User': User}
