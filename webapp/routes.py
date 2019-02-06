fbf96670308b0bc43d3b6c3dfb6d3e7933c1cb56
Merlin Webster <mjftwebster@gmail.com>
2019-02-06 01:30:14 +0000

    Added start of brewcontrol API nodes control panel

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from random import randint
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
@@ -96,6 +98,34 @@ def user(username=''):
 
     return render_template(
         'user.html', title='Account', user=user, edit=edit, form=form)
+
+@app.route('/brewcontrol')
+@login_required
+def brewcontrol():
+    if not current_user.is_admin:
+        return redirect(url_for('index'))
+    node_urls = [
+        'http://192.168.0.35:5010',
+        'http://192.168.0.35:5020',
+        'http://192.168.0.35:5030'
+    ]
+    api_nodes = []
+    for u in node_urls:
+        r = requests.get(u)
+        t = r.text
+        if t:
+            api_nodes.append(json.loads(r.text))
+    return render_template('brewcontrol.html', title='Brewing Control Panel', nodes=api_nodes)
+
+#TODO: Impliment a way of making API calls from within brewing control panel
+# @app.context_processor
+# def utility_processor():
+#     def call_api_node(host, port, method):
+#         if host and port and method:
+#             url = "http://{}:{}/{}".format(host, port, method)
+#             r = requests.post(url)
+#             print('Made API call: {}. Method returned: {}'.format(url, r.text))
+#             return(r.text)
 
 # Dummy route is here so that we can link to the flask-admin page
 @app.route('/admin')
